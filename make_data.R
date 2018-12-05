425d5cc779d59fb272b47fdc3861c66ff7fcb4e9
Joris Wiethase <joriswiethase@Joriss-iMac.lan>
2018-12-05 12:53:08 +0000

    Add eltontrait information

--- make_data.R
+++ make_data.R
@@ -13,22 +13,22 @@ data <- read.csv("OpWall band data all.csv", header=T, na.strings=c("","NA", "-"
 # Read in file containing the mist netting locations
 locations <- read.csv("Sites coordinates.csv") %>% 
   dplyr::rename(Loc = Abbreviation, Latitude = lat, Longitude = lon) %>% 
-  select(-"Site")
+  dplyr::select(-"Site")
 
 # Subset raw data into individual years. Data had different format from year to year, so it needs to be edited separately
     # Standardize each subset
 d2014 <- data %>% 
-  filter(field.season == 2014) %>% 
-  mutate(DateTime = ymd_hm(paste(Date, Time, sep= " "))) %>% 
-  select(-one_of(c('Date', 'Time', 'Band.size'))) %>% 
-  mutate(Recap. = ifelse(is.na(Recap.), "N", "Y"),  
+  dplyr::filter(field.season == 2014) %>% 
+  dplyr::mutate(DateTime = ymd_hm(paste(Date, Time, sep= " "))) %>% 
+  dplyr::select(-one_of(c('Date', 'Time', 'Band.size'))) %>% 
+  dplyr::mutate(Recap. = ifelse(is.na(Recap.), "N", "Y"),  
          Sex = substr(Sex, start = 1, stop = 1),   
          Sexing.method = substr(Sexing.method, start = 1, stop = 1),   
          Age = substr(Age, start = 1, stop = 1))    
 
 d2015 <- data %>% 
-  filter(field.season == 2015) %>% 
-  mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
+  dplyr::filter(field.season == 2015) %>% 
+  dplyr::mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
                                          Time = gsub("(\\d{2})(?=\\d{2})", "\\1:", Time, perl = TRUE),
                                          DateTime = ymd_hm(paste(Date, Time, sep= " ")),
          Band.size = stringr::str_pad(Band.size, 2, side = "left", pad = "0"),
@@ -36,54 +36,61 @@ d2015 <- data %>%
          Band.ID = paste(Band.size, Band.ID, sep = "-"),
          Sexing.method = sapply(strsplit(as.character(Sex), "/"), "[", 2),
          Sex = sapply(strsplit(as.character(Sex), "/"), "[", 1)) %>% 
-  select(-one_of(c('Date', 'Time', 'Band.size')))
+  dplyr::select(-one_of(c('Date', 'Time', 'Band.size')))
 
 d2016 <- data %>% 
-  filter(field.season == 2016) %>% 
-  mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
+  dplyr::filter(field.season == 2016) %>% 
+  dplyr::mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
          Time = gsub("(\\d{2})(?=\\d{2})", "\\1:", Time, perl = TRUE),
          DateTime = ymd_hm(paste(Date, Time, sep= " ")),
          Sexing.method = sapply(strsplit(as.character(Sex), "/"), "[", 2),
          Sex = sapply(strsplit(as.character(Sex), "/"), "[", 1)) %>% 
-  select(-one_of(c('Date', 'Time', 'Band.size')))
+  dplyr::select(-one_of(c('Date', 'Time', 'Band.size')))
 
 d2017 <- data %>% 
-  filter(field.season == 2017) %>% 
-  mutate(DateTime = ymd_hm(paste(Date, Time, sep= " ")),
+  dplyr::filter(field.season == 2017) %>% 
+  dplyr::mutate(DateTime = ymd_hm(paste(Date, Time, sep= " ")),
          Sexing.method = gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Sex, perl=T),
          Method.Age. = gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Sex, perl=T)) %>% 
-  mutate(Sex = gsub("\\([^()]*\\)", "", Sex), Age = gsub("\\([^()]*\\)", "", Age)) %>% 
-  select(-one_of(c('Date', 'Time', 'Band.size')))
+  dplyr::mutate(Sex = gsub("\\([^()]*\\)", "", Sex), Age = gsub("\\([^()]*\\)", "", Age)) %>% 
+  dplyr::select(-one_of(c('Date', 'Time', 'Band.size')))
 
 d2018 <- data %>% 
-  filter(field.season == 2018) %>% 
-  mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
+  dplyr::filter(field.season == 2018) %>% 
+  dplyr::mutate(Time = sprintf("%04d", as.numeric(as.character(Time))),
          Time = gsub("(\\d{2})(?=\\d{2})", "\\1:", Time, perl = TRUE),
          DateTime = ymd_hm(paste(Date, Time, sep= " ")),
          Sexing.method = sapply(strsplit(as.character(Sex), "/"), "[", 2),
          Sex = sapply(strsplit(as.character(Sex), "/"), "[", 1),
          Net = as.numeric(as.character(Net))) %>% 
-  select(-one_of(c('Date', 'Time', 'Band.size')))
+  dplyr::select(-one_of(c('Date', 'Time', 'Band.size')))
 
 # Re-combine the subsets
 data <- rbind(d2014, d2015, d2016, d2017, d2018) %>% 
-  select(DateTime, field.season, everything()) 
+  dplyr::select(DateTime, field.season, everything()) 
 
 # Add location information to the main data set 
 data <- merge(data, locations, by = "Loc", all.x = TRUE) %>% 
   dplyr::rename(Site = Loc) %>% 
-  mutate(Date = date(DateTime))
+  dplyr::mutate(Date = date(DateTime))
 
-# Correct BAnd.ID and Species errors in th emain data set 
+# Correct Band.ID and Species errors in th emain data set 
 data$Band.ID <- as.character(data$Band.ID)
 data$Species <- as.character(data$Species)
 data$Band.ID[data$Band.ID == "01-440"] <- "0A-440"
-data$Species[data$Species == "Cercomacra tyrannina"] <- "Cercomacra tyrannia"
+data$Species[data$Species == "Cercomacra tyrannia"] <- "Cercomacra tyrannina"
 data$Species[data$Species == "Formicarios analis"] <- "Formicarius analis"
 data$Species[data$Species == "Isteria guttata"] <- "Isleria guttata"
 data$Species[data$Species == "Mionectes macconelli"] <- "Mionectes macconnelli"
 data$Species[data$Species == "Onycorhynchus coronatus"] <- "Onychorhynchus coronatus"
 data$Species[data$Species == "Platyrincus platyrhynchos"] <- "Platyrinchus platyrhynchos"
+data$Species[data$Species == "Platyrhynchus platyrhinchos"] <- "Platyrinchus platyrhynchos"
+data$Species[data$Species == "Glyphorhynchus spirurus"] <- "Glyphorynchus spirurus"
+data$Species[data$Species == "Myrmoderus ferruginea"] <- "Myrmoderus ferrugineus"
+data$Species[data$Species == "Phoenicercus carnifex"] <- "Phoenicircus carnifex"
+data$Species[data$Species == "Willisornis poeciliontus"] <- "Willisornis poecilinotus"
+
+
 data$Species[data$Species == "Platyrincus saturatus"] <- "Platyrinchus saturatus"
 
 data$Species[data$Species == "Hylophilus ochraceiceps (?)"] <- "Tunchiornis ochraceiceps"
@@ -105,7 +112,6 @@ data$Species[data$Band.ID == "0A-206" & data$Species == "Platyrinchus mystaceus"
 data$Species[data$Band.ID == "1B-027" & data$Species == "Xiphorhynchus obsoletus"] <- "Xiphorhynchus pardalotus"
 data$Species[data$Band.ID == "1B-169" & data$Species == "Xiphorhynchus guttatus"] <- "Xiphorhynchus pardalotus"
 data$Species[data$Band.ID == "1B-186" & data$Species == "Xiphorhynchus guttatus"] <- "Xiphorhynchus pardalotus"
-
 
 data <- data %>% 
   dplyr::rename(Recap = Recap.)
@@ -134,12 +140,20 @@ to_join$Site <- as.character(to_join$Site)
 
 # Merge effort sheet with main data set, standardise species and capture numbers by 100 mist netting hours
 data.set <- merge(data, to_join, by = c("Site", "Date"), all.x=T) %>% 
-  group_by(Date, Site) %>% 
-  mutate(species_100hours = length(unique(Species))*100/total_net_hours_day,
+  dplyr::group_by(Date, Site) %>% 
+  dplyr::mutate(species_100hours = length(unique(Species))*100/total_net_hours_day,
          captures_100hours = n()*100/total_net_hours_day)
 
 # Check which lines don't have information for total_net_hours_day 
 View(data.set[is.na(data.set$total_net_hours_day),])
 
-write.csv(data.set, "all_joined.csv")
+# Add EltonTraits 1.0: Species‐level foraging attributes
+
+eltontraits <- read.csv("eltontraits.csv") %>% 
+  dplyr::select(-c(Diet.EnteredBy, ForStrat.EnteredBy))
+
+test2 <- merge(data.set, eltontraits, by = "Species", all.x = TRUE, all.y = FALSE) %>% unique()
+
+
+write.csv(test2, "all_joined.csv")
                   
